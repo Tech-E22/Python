9f2753f82ae6f208000dd4b7223ca0378678e277
Erika Anderson <emanderson090713@gmail.com>
2022-08-29 00:55:20 +0000

    edits

--- Possible_code_AWS_List.py
+++ Possible_code_AWS_List.py
@@ -1,15 +1,25 @@
-#notes at this point. lol
+"""
+    Program:
+    Create a list of services using Python. IE: S3, Lambda, EC2, etc
+    2. The list should be empty initially.
+    3. Populate the list using append or insert.
+    4. Print the list and the length of the list.
+    5. Remove two specific services from the list by name or by index.
+    6. Print the new list and the new length of the list.
+
+"""
 
 service_list = []
 
 service_list.append("EC2")
 service_list.append("Lambda")
-service_list.append("ECS")
+service_list.append("DynamoDB")
 service_list.append("S3")
-service_list.append("CodeBuild")
+
 
 print(service_list)
 print(len(service_list))
+
 
 
 # Remove by name
