3a4a8136737a9d5e910081936bbca3f0b9bd89fd
Erika Anderson <emanderson090713@gmail.com>
2022-09-05 16:48:13 +0000

    Automation to create S3 in us-east-1

--- S3_bucket_boto3.py
+++ S3_bucket_boto3.py
@@ -1,11 +1,9 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("esoflippinflybucket")
+bucket=aws_resource.Bucket("esoflippinflybucket1")
 response = bucket.create(
     ACL='public-read',
-    CreateBucketConfiguration={
-        'LocationConstraint':'us-east-2'
-    },
+  
    
  )
   
