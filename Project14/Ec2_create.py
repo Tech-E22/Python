52e9f0b558e1820b9c3e31602575cc5fb8c1c5c9
Erika Anderson <emanderson090713@gmail.com>
2022-09-11 16:01:42 +0000

    ec2 basic script

--- Project14/Ec2_create.py
+++ Project14/Ec2_create.py
@@ -6,4 +6,28 @@ ec2.create_instances(
     InstanceType='t2.micro',
     MaxCount=3,
     MinCount=3,
-)
+    TagSpecifications=[
+            {
+                    'ResourceType': 'instance',
+                    'Tags' : [{"Key": "Environment", "Value": "Prod"}]
+                    
+                                },
+            ],
+            
+        )
+    
+instance= ec2.create_instances(
+        ImageId="ami-05fa00d4c63e32376",
+        InstanceType="t2.micro",
+        MaxCount=3,
+        MinCount=3,
+        TagSpecifications=[
+            {
+                    'ResourceType': 'instance',
+                    'Tags' : [{"Key": "Environment", "Value": "Prod"}]
+                    
+                                },
+            ],
+            
+        )
+print ("EC2 instances have been generated successfully")
