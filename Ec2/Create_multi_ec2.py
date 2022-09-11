e4e10d07c8cd81bf37d9637382e6ba31c74bab1e
Erika Anderson <emanderson090713@gmail.com>
2022-09-11 11:03:16 +0000

    multi ec2

---
+++ Ec2/Create_multi_ec2.py
@@ -0,0 +1,7 @@
+import boto3
+    ec2_resource=boto3.resource("ec2")
+    ec2_resource.create_instances(ImageId='',
+        InstanceType='t2.micro',
+        MaxCount=3,
+        MinCount=3
+        )
