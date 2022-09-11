b4ae9ace2948f7affbabeeb513c6fd83dba40489
Erika Anderson <emanderson090713@gmail.com>
2022-09-11 00:40:06 +0000

    Edited

--- S3/Upload_file_S3.py
+++ S3/Upload_file_S3.py
@@ -1,6 +1,24 @@
 import boto3
 s3_resource=boto3.client("s3")
 s3_resource.upload_file(
-    Filename="upload.png",
+    Filename="/Users/erika/Desktop/test.jpg",
     Bucket="Esoflippinflybucket1",
-    Key="uploadtest.png")
+    Key="uploadtest.png"
+)
+
+#upload multiple files
+
+import os
+import glob
+
+cwd=os.getcwd()
+cwd=cwd+"/upload/"
+files=glob.glob(cwd+"*.jpg")
+
+for file in files:
+    s3=boto3.client('s3')
+    s3.upload_file(
+    Filename=file,
+    Bucket="boto3-luit-jl7392384",
+    Key=file.split("/")[-1]
+    )
